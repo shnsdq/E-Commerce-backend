63039449567d8b7311c71913b623b1b34dbe7c18
Shaheen <shn.sdq29@gmail.com>
2025-07-28 01:26:26 +0530

    commit

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ import cors from 'cors'
 import 'dotenv/config'
 import connectDB from './config/mongodb.js'
 import connectCloudinary from './config/cloudinary.js'
+import userRouter from './routes/userRoutes.js'
+import productRouter from './routes/productRoute.js'
 
 //App Config
 const app = express()
@@ -15,6 +17,8 @@ app.use(express.json())
 app.use(cors())
 
 // api endpoints
+app.use('/api/user',userRouter)
+app.use('/api/product',productRouter)
 
 app.get('/', (req,res)=>{
     res.send("API is working")
