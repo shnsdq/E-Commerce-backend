aa7eb2ec4835bec6325f6673f83dbec337694dc1
Shaheen <shn.sdq29@gmail.com>
2025-07-29 16:49:00 +0530

    commit

---
+++ controllers/cartController.js
@@ -0,0 +1,26 @@
+
+
+//add product to user cart
+const addToCart = async (req,res) => {
+    try {
+        
+
+        
+    } catch (error) {
+        
+    }
+}
+
+
+//update user cart
+const updateCart = async (req,res) => {
+
+}
+
+
+//get user cart data
+const getUserCart = async (req,res) => {
+
+}
+
+export {addToCart,updateCart,getUserCart}
